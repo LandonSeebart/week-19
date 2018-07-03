810ae8e12fce80658bc57735d4b6ab8f2a52a17d
landon <landon.seebart@gmail.com>
2018-07-02 19:27:43 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import Card from './components/Card'
 
 class App extends Component {
+  state = {
+    id: {},
+    url: {}
+  };
+  
   render() {
     return (
       <div className="App">
-        <Card />
+        <Card  />
       </div>
     );
   }
